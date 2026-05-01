3a90fd4335504e212a1772bbe48ca692824a4ecc
Beto956 <alex695215@gmail.com>
2026-05-01 13:44:27 -0500

    Force dynamic render for request details page

--- src/app/request-spot/details/page.tsx
+++ src/app/request-spot/details/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import DetailsPageContent from "./detailspagecontent";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<div style={{ padding: 20 }}>Loading...</div>}>
